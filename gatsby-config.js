5a7896f8b315068057181b656d3aad931df42f5d
Sarthak Narayan <sarthak.narayang@gmail.com>
2021-12-04 12:13:31 +0530

    Changed rss feed title and removed comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -154,12 +154,7 @@ module.exports = {
             }
           `,
             output: '/rss.xml',
-            title: 'RSS feed for website',
-            // optional configuration to insert feed reference in pages:
-            // if `string` is used, it will be used to create RegExp and then test if pathname of
-            // current page satisfied this regular expression;
-            // if not provided or `undefined`, all pages will have feed reference inserted
-            match: '^/blogs/',
+            title: "Sarthak Naryan's website rss feed",
             feed_url: 'https://sarthak-narayan.netlify.app/rss.xml',
             managingEditor: 'Sarthak Narayan',
             webMaster: 'Sarthak Narayan',
